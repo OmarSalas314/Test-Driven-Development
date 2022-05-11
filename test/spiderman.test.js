f967595cbe34d0a8ba4c62f1f6d5f8a30235b396
OmarSalas314 <omarsalasvieyra@gmail.com>
2022-05-11 10:53:56 -0600

    Agregando prueba dos con errores

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -4,12 +4,15 @@ describe("Test Tests fot Spiderman class", () => {
       //Aqui escribimos el codigo que queremos usar tal cual
       //Quiero poder intanciar un objeto Spiderman con esta info
       const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
-
       //Validamos que este codigo funcione de la forma esperada
-      expect(andrewGarfield.namemovie).toBe("Spiderman Sorny")
+      expect(andrewGarfield.namemovie).toBe("Spiderman Sony")
       expect(andrewGarfield.age).toBe(31)
-      expect(andrewGarfield.name).toBe("Andrew Garfieldddddddddddd")
-      expect(andrewGarfield.numbmov).toBe(22)
+      expect(andrewGarfield.name).toBe("Andrew Garfield")
+      expect(andrewGarfield.numbmov).toBe(2)
       expect(andrewGarfield.produc).toBe("Sony")
     });
+    test('2) Use the method getInfo()', () => {
+      const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+      expect(tomHolland.getInfo()).toBe("Spiderman Marvel")   
+    });    
   })
